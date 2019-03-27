792abf5ad94cfbd409809389b24f91ed59cc8e03
Jeffery Thompson <impulse985@gmail.com>
2019-03-26 22:52:07 -0400

    Remove unused usings

--- CivilizationDiscordWebhook/Startup.cs
+++ CivilizationDiscordWebhook/Startup.cs
@@ -1,13 +1,10 @@
 ﻿using Discord.Webhook;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System.IO;
-using System.Net;
-using System.Net.Http;
 
 namespace CivilizationDiscordWebhook
 {
